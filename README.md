8eefe50ed36c544628d670dfdd13026f12de4279
Scott.Lee <once_key@126.com>
2017-01-17 11:59:09 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,23 @@
+#项目介绍
+
+    1.提供公司内部人员管理系统
+    2.积分管理系统
+    3.项目跟踪管理系统
+    4.成本费用管理系统
+
+#依赖库
+
+    1. spring-boot-starter-web 
+    2. spring-boot-starter-jpa-extra
+    3. lombok
+
+#数据库
+
+    1. mysql
+   
+#TO-DO
+
+    1.集成权限
+    2.完成人员组织机构管理
+    3.rest接口文档 swagger
+    4.集成工作流
---
+++ src/main/java/com/scott/lee/DashBoardMgmtApplication.java
@@ -0,0 +1,20 @@
+package com.scott.lee;
+
+import com.slyak.spring.jpa.GenericJpaRepositoryFactoryBean;
+import com.slyak.spring.jpa.GenericJpaRepositoryImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"com.scott.lee"})
+@EnableJpaRepositories(basePackages = "com.scott.lee.**.repository",
+		repositoryBaseClass = GenericJpaRepositoryImpl.class,
+		repositoryFactoryBeanClass = GenericJpaRepositoryFactoryBean.class)
+public class DashBoardMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashBoardMgmtApplication.class, args);
+	}
+}
